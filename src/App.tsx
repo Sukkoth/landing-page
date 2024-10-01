e9863470c1d414961f80a8c2215131cabeef826e
Sukkoth <suukootj@gmail.com>
2024-10-01 16:41:29 +0300

    responsive form

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ export default function App() {
       <Features />
       <Carousel />
       <AccordionSection />
-      {/* <Form /> */}
+      <Form />
       <Footer />
     </main>
   );
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,11 +3,11 @@ import join from "../assets/images/join.png";
 
 function Form() {
   return (
-    <div className='mt-10'>
-      <div className='relative w-full h-[100dvh]'>
-        <img src={join} alt='join-img' className='object-cover h-full w-full' />
-        <div className='absolute inset-y-0 left-[80px] top-[140px]'>
-          <h1 className='text-[80px] w-[40rem]'>
+    <div className="mt-10">
+      <div className="relative h-[100dvh] w-full">
+        <img src={join} alt="join-img" className="h-full w-full object-cover" />
+        <div className="absolute inset-x-0 inset-y-0 top-[50px] px-[20px] xl:left-[80px] xl:top-[140px]">
+          <h1 className="w-[96%] text-[32px] xl:w-[40rem] xl:text-[80px]">
             Join the Fluid AI Waitlist Today!
           </h1>
           <JoinForm />
