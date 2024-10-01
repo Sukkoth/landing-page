cabf2e7377ca563bc02e5fbb68aabc0bb5ae48bf
Sukkoth <suukootj@gmail.com>
2024-10-01 15:52:12 +0300

    responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,21 +2,21 @@ import logo from "../assets/fluid-icon.svg";
 
 function Footer() {
   return (
-    <footer className='mt-10 p-[40px]'>
-      <div className='flex justify-between'>
+    <footer className="mt-10 p-[40px]">
+      <div className="flex flex-col justify-between gap-10 lg:flex-row">
         <div>
-          <img src={logo} alt='logo' className='w-[207px] h-[40px]' />
-          <p className='text-white/50 mt-6 ps-3'>2024 Fluid AI</p>
+          <img src={logo} alt="logo" className="h-[40px] w-[207px]" />
+          <p className="mt-3 ps-3 text-white/50 lg:mt-6">2024 Fluid AI</p>
         </div>
-        <div className='grid grid-cols-2 gap-20 text-white/50 text-[18px]'>
-          <div className='space-y-3'>
+        <div className="grid grid-cols-2 gap-20 text-[18px] text-white/50">
+          <div className="space-y-3">
             <p>Our Features</p>
             <p>Areas</p>
             <p>FAQs</p>
             <p>The Waitlist</p>
             <p>Contact Us</p>
           </div>
-          <div className='space-y-3'>
+          <div className="space-y-3">
             <p>Terms of service</p>
             <p>Privacy Policy</p>
           </div>
