37063e260f46f3dd9b9b386c4bf955f6bfa31377
Sukkoth <suukootj@gmail.com>
2024-10-02 18:04:14 +0300

    animated Home

--- src/components/Bright.tsx
+++ src/components/Bright.tsx
@@ -1,64 +1,8 @@
 import gradient from "../assets/gradient.svg";
-import todo from "../assets/cards/todo.svg";
-import todo2 from "../assets/cards/todo2.svg";
-import tab from "../assets/cards/tab.svg";
-import card from "../assets/cards/card.svg";
-import newTicket from "../assets/cards/new-ticket.svg";
-import tripIdeas from "../assets/cards/trip-ideas.svg";
-import bookFlight from "../assets/cards/book-flight.svg";
-import reservation from "../assets/cards/reservation.svg";
-import cafe from "../assets/cards/cafe.svg";
-import { motion } from "framer-motion";
 
 function Brigth() {
   return (
     <div className="min-h-[100dvh]">
-      {/* <img src={gradient} alt="gradient" className="w-full" /> */}
-      {/* <div className="">
-        <img src={todo} alt="todo" className="absolute top-[630px]" />
-        <img
-          src={tab}
-          alt="tab"
-          className="absolute left-[260px] top-[620px]"
-        />
-        <img
-          src={card}
-          alt="card"
-          className="absolute left-[197px] top-[400px]"
-        />
-
-        <img
-          src={cafe}
-          alt="cafe"
-          className="absolute left-[260px] top-[875px]"
-        />
-        <img
-          src={newTicket}
-          alt="new-ticket"
-          className="absolute left-[174px] top-[1035px]"
-        />
-        <img
-          src={todo2}
-          alt="todo-2"
-          className="absolute right-[240px] top-[342px]"
-        />
-        <img
-          src={bookFlight}
-          alt="bookFlight"
-          className="absolute right-0 top-[756px]"
-        />
-        <img
-          src={tripIdeas}
-          alt="tripIdeas"
-          className="absolute right-[50px] top-[820px]"
-        />
-        <img
-          src={reservation}
-          alt="reservation"
-          className="absolute right-[270px] top-[920px]"
-        />
-      </div> */}
-
       <img
         src={gradient}
         alt="gradient"
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,17 @@ import logo from "../assets/fluid-icon.svg";
 import openNavIcon from "../assets/nav-menu.svg";
 import closeNavIcon from "../assets/closeNav.svg";
 
+import todo from "../assets/cards/todo.svg";
+import todo2 from "../assets/cards/todo2.svg";
+import tab from "../assets/cards/tab.svg";
+import card from "../assets/cards/card.svg";
+import newTicket from "../assets/cards/new-ticket.svg";
+import tripIdeas from "../assets/cards/trip-ideas.svg";
+import bookFlight from "../assets/cards/book-flight.svg";
+import reservation from "../assets/cards/reservation.svg";
+import cafe from "../assets/cards/cafe.svg";
+import event from "../assets/cards/event.png";
+
 // import Intro from "./Intro";
 import Bright from "./Bright";
 import { useScroll, motion, useTransform } from "framer-motion";
@@ -15,10 +26,10 @@ function Home() {
     target: targetRef,
   });
 
-  const y = useTransform(scrollYProgress, [0, 1], ["1%", "-50%"]);
+  const y = useTransform(scrollYProgress, [0, 1], ["1%", "-80%"]);
 
   return (
-    <div className="mb-[20rem] h-[160vh] lg:mb-[60vh]" ref={targetRef}>
+    <div className="mb-[20rem] h-[230vh] lg:mb-[60vh]" ref={targetRef}>
       <div className="sticky top-0 h-screen">
         <div className="absolute top-0 z-[9999] grid h-[100dvh] w-full grid-rows-[auto_1fr]">
           {/* for larger devices */}
@@ -78,12 +89,12 @@ function Home() {
               style={{
                 height: useTransform(
                   scrollYProgress,
-                  [0, 0.7],
+                  [0, 0.3],
                   ["180px", "48px"],
                 ), // Adjust the minimum size as needed
                 width: useTransform(
                   scrollYProgress,
-                  [0, 0.7],
+                  [0, 0.3],
                   ["600px", "200px"],
                 ), // Adjust the minimum size as needed
                 maxWidth: "100%", // Ensure the logo doesn't exceed the container width on mobile
@@ -98,10 +109,10 @@ function Home() {
           <motion.div
             className="flex items-center justify-center text-center"
             style={{
-              opacity: useTransform(scrollYProgress, [0, 0.7], [0, 1]),
+              opacity: useTransform(scrollYProgress, [0, 0.3], [0, 1]),
               translateY: useTransform(
                 scrollYProgress,
-                [0, 0.7],
+                [0, 0.3],
                 ["100%", "0%"],
               ),
             }}
@@ -116,6 +127,190 @@ function Home() {
             </div>
           </motion.div>
         </div>
+
+        <div className="">
+          {/* <motion.img
+            src={todo}
+            alt="todo"
+            className="absolute z-[999]"
+            style={{
+              top: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["430px", "430px"],
+              ),
+              left: useTransform(scrollYProgress, [0.3, 0.6], ["0px", "5px"]),
+              opacity: useTransform(scrollYProgress, [0.3, 0.6], [0, 1]),
+            }}
+          /> */}
+
+          <motion.img
+            id="cafe"
+            src={tab}
+            alt="tab"
+            className="absolute z-[999]"
+            style={{
+              left: useTransform(scrollYProgress, [0.3, 0.6], ["0px", "460px"]),
+              top: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["680px", "680px"],
+              ),
+              opacity: useTransform(scrollYProgress, [0.3, 0.6], [0, 1]),
+            }}
+          />
+
+          <motion.img
+            id="clapDubai"
+            src={card}
+            alt="card"
+            className="absolute z-[999]"
+            style={{
+              left: useTransform(scrollYProgress, [0.3, 0.6], ["0px", "197px"]),
+              top: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["130px", "130px"],
+              ),
+              opacity: useTransform(scrollYProgress, [0.3, 0.6], [0, 1]),
+            }}
+          />
+
+          <motion.img
+            id="renew-car-insurance"
+            src={todo2}
+            alt="todo-2"
+            className="absolute z-[999]"
+            style={{
+              left: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["-800px", "-60px"],
+              ),
+              top: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["440px", "440px"],
+              ),
+              opacity: useTransform(scrollYProgress, [0.3, 0.6], [0, 1]),
+            }}
+          />
+          <motion.img
+            id="event-left"
+            src={event}
+            alt="todo-2"
+            className="absolute z-[999]"
+            style={{
+              left: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["-400px", "0px"],
+              ),
+              top: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["580px", "580px"],
+              ),
+              opacity: useTransform(scrollYProgress, [0.3, 0.6], [0, 1]),
+            }}
+          />
+
+          <motion.img
+            id="newTicket"
+            src={newTicket}
+            alt="new-ticket"
+            className="absolute z-[999]"
+            style={{
+              left: useTransform(scrollYProgress, [0.3, 0.6], ["0", "174px"]),
+              top: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["785px", "785px"],
+              ),
+              opacity: useTransform(scrollYProgress, [0.3, 0.6], [0, 1]),
+            }}
+          />
+
+          <motion.img
+            id="renew-car-insurance"
+            src={todo2}
+            alt="todo-2"
+            className="absolute z-[999]"
+            style={{
+              right: useTransform(scrollYProgress, [0.3, 0.6], ["0", "340px"]),
+              top: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["142px", "142px"],
+              ),
+              opacity: useTransform(scrollYProgress, [0.3, 0.6], [0, 1]),
+            }}
+          />
+
+          <motion.img
+            id="event-right"
+            src={event}
+            alt="todo-2"
+            className="absolute z-[999]"
+            style={{
+              right: useTransform(scrollYProgress, [0.3, 0.6], ["0", "170px"]),
+              top: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["260px", "260px"],
+              ),
+              opacity: useTransform(scrollYProgress, [0.3, 0.6], [0, 1]),
+            }}
+          />
+
+          <motion.img
+            id="book-a-flight"
+            src={bookFlight}
+            alt="bookFlight"
+            className="absolute z-[999]"
+            style={{
+              right: useTransform(scrollYProgress, [0.3, 0.6], ["0", "10px"]),
+              top: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["520px", "520px"],
+              ),
+              opacity: useTransform(scrollYProgress, [0.3, 0.6], [0, 1]),
+            }}
+          />
+
+          <motion.img
+            id="trip-ideas"
+            src={tripIdeas}
+            alt="tripIdeas"
+            className="absolute z-[999]"
+            style={{
+              right: useTransform(scrollYProgress, [0.3, 0.6], ["0px", "80px"]),
+              top: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["600px", "600px"],
+              ),
+              opacity: useTransform(scrollYProgress, [0.3, 0.6], [0, 1]),
+            }}
+          />
+
+          <motion.img
+            src={reservation}
+            alt="reservation"
+            className="absolute z-[999]"
+            style={{
+              right: useTransform(scrollYProgress, [0.3, 0.6], ["0", "270px"]),
+              top: useTransform(
+                scrollYProgress,
+                [0.3, 0.6],
+                ["660px", "660px"],
+              ),
+              opacity: useTransform(scrollYProgress, [0.3, 0.6], [0, 1]),
+            }}
+          />
+        </div>
+
         <div className="h-[35rem]"></div>
         <motion.div style={{ y }} className="overflow-hidden">
           <Bright />
